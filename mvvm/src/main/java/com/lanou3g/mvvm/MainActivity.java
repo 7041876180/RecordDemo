4985f64d320140c80a4fd805c6a0cfc206618f97
武奇 <risky5200@gmail.com>
2016-04-02 20:57:12 +0800

    简单试验一下

--- mvvm/src/main/java/com/lanou3g/mvvm/MainActivity.java
+++ mvvm/src/main/java/com/lanou3g/mvvm/MainActivity.java
@@ -1,13 +1,20 @@
 package com.lanou3g.mvvm;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lanou3g.mvvm.databinding.ActivityMainBinding;
+import com.lanou3g.mvvm.vm.User;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        User user = new User("天使","277");
+        binding.setUser(user);
     }
 }
